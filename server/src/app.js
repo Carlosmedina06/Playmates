4bea61bd87937c894de854f4653dc496c27bf38f
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-23 00:58:38 -0300

    cambios

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import http from 'http'
+
+import express from 'express'
+import cors from 'cors'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import morgan from 'morgan'
+
+export async function createServer(typeDefs, resolvers) {
+  const app = express()
+  const serverHttp = http.createServer(app)
+  const server = new ApolloServer({ typeDefs, resolvers })
+
+  await server.start()
+
+  app.use('/graphql', morgan('dev'), cors(), express.json(), expressMiddleware(server))
+
+  await new Promise((resolve) => serverHttp.listen({ port: 4000 }, resolve))
+  // eslint-disable-next-line no-console
+  console.log(`🚀 Server ready at http://localhost:4000`)
+}
