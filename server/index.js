3d017a7bcc6525538e506c11ad8f1ca1a746bd7f
carlosmedina06 <slaawmedina14@gmail.com>
2023-02-23 01:04:42 -0300

    cambio carpetas resolvers, typedefs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import { createServer } from '../server/src/app.js'
 
-import { typeDefs } from './src/graphql/typedefs.js'
-import { resolvers } from './src/graphql/resolvers.js'
+import { typeDefs } from './src/graphql/TypeDefs/typedefs.js'
+import { resolvers } from './src/graphql/Resolvers/resolvers.js'
 import { connect } from './src/db.js'
 
 createServer(typeDefs, resolvers)
