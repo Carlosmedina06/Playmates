986f8c726d643ae1f9fe63f3222aff1e8673e921
jordanvargom <jordanvargom@gmail.com>
2023-02-23 02:16:01 -0300

    Modelo de Comentarios

---
+++ server/src/models/Comentarios.js
@@ -0,0 +1,41 @@
+import moongose from 'mongoose'
+
+const comentarioSchema = new moongose.Schema({
+  contenido: String,
+  media: String,
+  fechaCreacion: {
+    type: Date,
+    default: Date.now,
+  },
+  fechaModificacion: {
+    type: Date,
+  },
+  restado: {
+    type: Boolean,
+    default: true,
+  },
+  usuario: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+  },
+  pulicacion: {
+    type: moongose.Schema.Types.ObjectId,
+    ref: 'Publicacion',
+  },
+  reportes: {
+    type: Number,
+    default: 0,
+  },
+})
+
+comentarioSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Comentario = moongose.model('Comentario', comentarioSchema)
+
+export default Comentario
